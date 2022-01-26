bd0b3e190174d9254dccb0eff56229e40bf6e134
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2022-01-27 00:15:59 +0200

    Add custom implementation for map, filter and reduce

---
+++ myMap.js
@@ -0,0 +1,20 @@
+/**
+ *
+  The map() method creates a new array with the results of calling a provided function on every element in the calling array
+  Callback is invoked with three arguments:
+  - the value of the element
+  - the index of the element
+  - the Array object being traversed
+*/
+
+Array.prototype.myMap = function(callback, thisArg) {
+  var mapArray = [];
+  for (let i = 0; i < this.length; i++) {
+      mapArray.push(callback.call(thisArg, this[i], i, this));
+  }
+  return mapArray;
+};
+
+const arr = [1, 2, 3];
+
+console.log(arr.myMap(item => item * 2)); // [2, 4, 6]
