74af5dc504966596ea19d0ac18b41089e1e43e4b
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2023-03-21 14:54:20 +0200

    more binary examples

--- toBinary.js
+++ toBinary.js
@@ -2,6 +2,8 @@ function toBinary(num) {
   return num.toString(2);
 }
 
+console.log(toBinary(2)); // 10
+console.log(toBinary(3)); // 11
 console.log(toBinary(4)); // 100
 console.log(toBinary(9)); // 1001
 console.log(toBinary(21)); // 10101
