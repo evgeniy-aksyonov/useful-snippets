3f4b9838e14d985545129dca0d029307e9b47d80
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2023-03-21 14:51:56 +0200

    add num to binary snippet

--- sleep.js
+++ sleep.js
@@ -1,11 +1,11 @@
 // sleep/delay function
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
 
 // or arrow func oneliner:
-// const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // Usage:
 
@@ -14,22 +14,20 @@ sleep(2000).then(() => console.log('World!'));
 
 // OR
 
-console.log('Hello')
+console.log('Hello');
 sleep(2000)
   .then(() => console.log('world!'))
   .then(() => sleep(2000))
-  .then(() => console.log('Goodbye!'))
+  .then(() => console.log('Goodbye!'));
 
 // OR with async await
 
-
-
 async function delayedGreeting() {
-  console.log("Hello");
+  console.log('Hello');
   await sleep(2000);
-  console.log("World!");
+  console.log('World!');
   await sleep(2000);
-  console.log("Goodbye!");
+  console.log('Goodbye!');
 }
 
 delayedGreeting();
@@ -37,10 +35,10 @@ delayedGreeting();
 // one more variant of calling a func
 
 async function delayedGreeting() {
-  console.log("Hello");
+  console.log('Hello');
   await sleep(2000);
-  console.log("World!");
+  console.log('World!');
 }
 
 delayedGreeting();
-console.log("Goodbye!");
+console.log('Goodbye!');
---
+++ toBinary.js
@@ -0,0 +1,7 @@
+function toBinary(num) {
+  return num.toString(2);
+}
+
+console.log(toBinary(4)); // 100
+console.log(toBinary(9)); // 1001
+console.log(toBinary(21)); // 10101
