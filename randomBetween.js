0fff122164fc946424b3aca18e29ead6af4baccd
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2025-07-26 14:08:20 +0200

    update simple randomInteger func example

--- randomBetween.js
+++ randomBetween.js
@@ -1,8 +1,8 @@
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
-function randomBetween(min, max) {
-  return Math.random() * (max - min) + min;
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // console.log(randomBetween(1, 10));
